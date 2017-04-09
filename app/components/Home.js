d46ad97e4452b934e7335297871cfc048eded23a
Lee <lee@leesander.com>
2017-04-08 21:09:53 -0500

    setup working

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,10 +4,13 @@ import React, { Component } from 'react';
 import styles from './Home.css';
 
 
+
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <button onClick={getToken}>Get Token</button>
+        <button onClick={sendCall}>Make call</button>
         <div className={styles.container} data-tid="container" />
       </div>
     );
@@ -21,6 +24,15 @@ async function getToken() {
   });
 
   twilio_token = await request.json();
+  console.log(twilio_token.token);
+  Twilio.Device.setup(twilio_token.token);
+  console.log(twilio_token);
+}
 
+Twilio.Device.ready(() => {
+  console.log('Its ready');
+});
+
+function sendCall() {
   console.log(twilio_token);
 }
