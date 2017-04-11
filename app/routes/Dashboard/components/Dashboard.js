78da116c4223fc4498543ba26bcbca09b25a6e83
mariot <mariotjimenez@gmail.com>
2017-04-10 19:31:59 -0500

    Updated dashboard.js and added css file

--- app/routes/Dashboard/components/Dashboard.js
+++ app/routes/Dashboard/components/Dashboard.js
@@ -1,11 +1,14 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './Dashboard.css';
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        Dashboard
+        <div className={styles.container} data-tid="container" />
+        <div>hello world!</div>
+        <div>hello world!</div>
       </div>
     );
   }
