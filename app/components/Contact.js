caf6429fa56699ee6a36a9010bbb9df61e33f82e
kevintaing86 <kevin.taing@ttu.edu>
2017-04-14 00:46:22 -0500

    Created dummy contacts
    Made mdl-list elements and assigned dummy contacts

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -2,15 +2,16 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router';
 import styles from './Contact.css';
-
+import {ListItem, ListItemContent, ListItemAction, Icon} from 'react-mdl';
 
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-      <h4>Contact</h4>
-      </div>
+      <ListItem>
+        <ListItemContent avatar="person">{this.props.contact.name}</ListItemContent>
+        <ListItemAction><Icon name="call"/></ListItemAction>
+      </ListItem>
     );
   }
 }
--- app/containers/ContactsPage.js
+++ app/containers/ContactsPage.js
@@ -1,11 +1,32 @@
 // @flow
 import React, { Component } from 'react';
 import Contact from '../components/Contact';
+import {List} from 'react-mdl';
 
 export default class ContactsPage extends Component {
   render() {
     return (
-      <Contact />
+      <List>
+        <Contact contact={contacts[0]}/>
+        <Contact contact={contacts[1]}/>
+        <Contact contact={contacts[2]}/>
+      </List>
     );
   }
 }
+
+// dummy data for Contacts
+var contacts = [
+  {
+    name: "Kevin",
+    number: "123"
+  },
+  {
+    name: "Lee",
+    number: "123"
+  },
+  {
+    name: "Mario",
+    number: "123"
+  }
+];
