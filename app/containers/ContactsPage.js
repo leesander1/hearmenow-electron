ade583cd385e26854390f42c30d8bac4c526e143
kevintaing86 <kevin.taing@ttu.edu>
2017-04-15 01:03:32 -0500

    Added search bar and created sub-folder for contancts
    Style on the search bar needs some improvements

--- app/containers/ContactsPage.js
+++ app/containers/ContactsPage.js
@@ -1,14 +1,18 @@
 // @flow
 import React, { Component } from 'react';
-import Contact from '../components/Contact';
-import {List} from 'react-mdl';
+import Contact from '../components/Contact/Contact';
+import SearchBar from '../components/Contact/SearchBar';
+import {List, Textfield} from 'react-mdl';
 
 export default class ContactsPage extends Component {
   render() {
     return (
-      <List>
-        <Contact contacts={contacts}/>
-      </List>
+      <div>
+        <SearchBar />
+        <List>
+          <Contact contacts={contacts}/>
+        </List>
+      </div>
     );
   }
 }
