b8db7ab700c4f7171c7eed38b3f89bcc5ffadf82
Cbellando <caleb.bellando@ttu.edu>
2017-04-28 14:52:01 -0500

    added debouncing to search bar

--- app/components/Contact/SearchBar.js
+++ app/components/Contact/SearchBar.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import styles from './Contact.css';
-import {Textfield} from 'react-mdl';
+import TextField from 'material-ui/TextField';
+import DebounceInput from 'react-debounce-input';
+
 
 
 export default class SearchBar extends Component{
+
   render(){
     return(
-      <Textfield
-        label="Search"
-        className={styles.searchBar}
-      />
+      <div>
+        <SearchApp />
+      </div>
     );
   }
-}
+  }
+
+var SearchApp = React.createClass({
+  getInitialState() {
+    return {
+      value: ''
+    };
+  },
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  },
+
+  render(){
+    return(
+      <div>
+        <DebounceInput
+            element={TextField}
+            hintText="Search"
+            fullWidth={true}
+            className={styles.searchBar}
+            minLength={1}
+            debounceTimeout={300}
+            onChange={event => this.handleChange(event)}/>
+          <p>Value: {this.state.value}</p>
+      </div>
+    );
+  }
+});
