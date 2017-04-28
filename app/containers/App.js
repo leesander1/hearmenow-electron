cd2f13209a7362e441c3999e9b0980610ee5c80e
Lee <lee@leesander.com>
2017-04-27 20:29:14 -0500

    added material ui

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,21 +1,33 @@
 // @flow
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer/Footer';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Layout, Header, HeaderRow, Content } from 'react-mdl'
+
+injectTapEventPlugin();
 
 export default class App extends Component {
   props: {
     children: HTMLElement
   };
-
   render() {
     return (
       <div className="">
-        <NavBar />
-        <div className="content">
-          {this.props.children}
-        </div>
+        <MuiThemeProvider>
+          <div>
+            <NavBar />
+            <div className="content">
+              {this.props.children}
+            </div>
+            <Footer />
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
+
+var connection = window.navigator.onLine
