02e798565fd49621e75738dfd91a5c2fb315ec0a
Cbellando <caleb.bellando@ttu.edu>
2017-05-06 18:11:40 -0500

    Contact search is now fully working

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,13 +1,46 @@
+// @flow
 import React, { Component } from 'react';
 // import { Link } from 'react-router';
 import styles from './Contact.css';
+import Avatar from 'material-ui/Avatar';
+import {ListItem} from 'material-ui/List';
+import FontIcon from 'material-ui/FontIcon';
+import {red500, green500, cyan500, blue500, pinkA200} from 'material-ui/styles/colors';
+import SearchBar from './Contact/SearchBar';
+import ContactList from './Contact/ContactList';
 
-export default class Contact extends Component {
-  render() {
-    return (
+export default class Contact extends Component{
+
+  render(){
+    return(
       <div>
-        <h4>Contact</h4>
+        <ContactApp />
       </div>
     );
   }
-}
+  }
+
+  export var ContactApp = React.createClass({
+      getInitialState() {
+        return {
+          value: ''
+        };
+      },
+
+      handleSearchInput(value) {
+        this.setState({value: value})
+      },
+
+      render(){
+        return(
+          <div>
+
+          <SearchBar
+          handleOnChange={this.handleSearchInput} />
+          <ContactList
+          searchTerm={this.state.value}/>
+
+          </div>
+        );
+      }
+    });
