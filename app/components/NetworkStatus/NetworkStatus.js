7d02d0d4be24e683bc69340aeb66553b0101e57a
lee <lee@leesander.com>
2017-05-10 15:28:12 -0500

    snackbar

--- app/components/NetworkStatus/NetworkStatus.js
+++ app/components/NetworkStatus/NetworkStatus.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
 import styles from './NetworkStatus.css';
+import Snackbar from 'material-ui/Snackbar';
 import FontIcon from 'material-ui/FontIcon';
 import {red500, green500, cyan500, blue500, pinkA200} from 'material-ui/styles/colors';
 import notifier from 'notifier';
@@ -15,7 +16,7 @@ export default class NetworkStatus extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {connection: true};
+    this.state = {connection: true, online_open: false, offline_open: false};
   }
   componentDidMount() {
     this.refreshID = setInterval(
@@ -27,20 +28,14 @@ export default class NetworkStatus extends React.Component {
     clearInterval(this.refreshID);
   }
   notifyOnline() {
-    nc.notify({
-      title: 'You are now Online!',
-      message: 'Hearmenow is now able to initiate or accept calls.',
-      closeLabel: 'Close',
-      wait: true
+    this.setState({
+      online_open: true
     });
     console.log("Online");
   }
   notifyOffline() {
-    nc.notify({
-      title: 'You are now Offline!',
-      message: 'Hearmenow is NOT able to initiate or accept calls while offline.',
-      closeLabel: 'Close',
-      wait: true
+    this.setState({
+      offline_open: true
     });
     console.log("Offline");
   }
@@ -61,11 +56,35 @@ export default class NetworkStatus extends React.Component {
     }
   }
 
+  handleOnlineRequestClose = () => {
+    this.setState({
+      online_open: false,
+    });
+  }
+
+  handleOfflineRequestClose = () => {
+    this.setState({
+      offline_open: false,
+    });
+  }
   trigger () {
     var x = this.state.connection ? nofityOnline() : notiftyOffline();
   }
   render() {
-    var input = this.state.connection ? <FontIcon className="material-icons" style={styles.icon} color={green500}>adjust</FontIcon> : <FontIcon className="material-icons" style={styles.icon} color={red500}>adjust</FontIcon>;
+    var input = this.state.connection ? <div><FontIcon className="material-icons" style={styles.icon} color={green500}>adjust</FontIcon>
+    <Snackbar
+      open={this.state.online_open}
+      message="✔️📶 You are now online 📶✔️"
+      autoHideDuration={2000}
+      onRequestClose={this.handleOnlineRequestClose}>
+    </Snackbar></div>:
+    <div><FontIcon className="material-icons" style={styles.icon} color={red500}>adjust</FontIcon>
+    <Snackbar
+      open={!this.state.offline_open}
+      message="❌📶 No internet connection 📶❌"
+      autoHideDuration={2000}
+      onRequestClose={this.handleOfflineRequestClose}>
+    </Snackbar></div>;
     return (
         <div>
          {input}
